07cb314faf64217bae314246e42303778d42e0bd
Mike Loecher <michael.w.loecher@gmail.com>
2025-07-15 20:29:25 -0700

    Reduced some error log levels that are slowing things down

--- gropt_dev/src/ils_bicgstabl.cpp
+++ gropt_dev/src/ils_bicgstabl.cpp
@@ -124,7 +124,7 @@ Eigen::VectorXd ILS_BiCGstabl::solve(Eigen::VectorXd &x_in)
         }
     }
 
-    spdlog::info("ILS_BiCGstabl::solve  rnorm0 = {:e}   rnorm = {:e}   ii = {:d}", rnorm0, rs[0].norm(), ii);
+    spdlog::debug("ILS_BiCGstabl::solve  rnorm0 = {:e}   rnorm = {:e}   ii = {:d}", rnorm0, rs[0].norm(), ii);
 
     stop_time = std::chrono::steady_clock::now();
     elapsed_us = stop_time - start_time;
