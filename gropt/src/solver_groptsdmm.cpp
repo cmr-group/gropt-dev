515c90213aad5467b0ceedf7140a49a3661c6a56
mloecher <michael.w.loecher@gmail.com>
2026-03-11 21:49:17 -0700

    typo

--- gropt/src/solver_groptsdmm.cpp
+++ gropt/src/solver_groptsdmm.cpp
@@ -94,7 +94,7 @@ SolveResult SolverGroptSDMM::solve(GroptParams &_gparams) {
 
         if ((logger(X) > 0) && (iiter > min_iter)) {
             if (extra_iters > 0) {
-                spdlog::info("First dsolved at iiter {:d}, now {:d} extra iterations", iiter, extra_iters);
+                spdlog::info("First solved at iiter {:d}, now {:d} extra iterations", iiter, extra_iters);
                 min_iter = iiter + extra_iters;
                 extra_iters = 0;
             } else {
