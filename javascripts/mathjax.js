081fa1f76a94653b55b8cfb018aa027edaa22c2a
mloecher <michael.w.loecher@gmail.com>
2026-03-14 18:07:41 -0700

    Deployed de37f5e with MkDocs version: 1.6.1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,26 @@
+window.MathJax = {
+  tex: {
+    tags: 'ams',
+    inlineMath: [["$", "$"], ["\\(", "\\)"]],
+    displayMath: [["$$", "$$"], ["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+    macros: {
+      tpose: "{T}",
+      reals: "\\mathbb{\\bf R}",
+      symm: "\\mathcal{S}",
+      eqdef: "="
+    }
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => { 
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
