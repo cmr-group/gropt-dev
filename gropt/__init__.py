186151494bb1f827b53141b7da0e81c9cbfc70b2
mloecher <michael.w.loecher@gmail.com>
2026-03-16 18:19:15 -0700

    Some changes to diffusion helpers

--- gropt/__init__.py
+++ gropt/__init__.py
@@ -1,9 +1,11 @@
 from importlib.metadata import version
-__version__ = version("gropt")
 
+__version__ = version('gropt')
+
+from . import readasc
 from .gropt_wrapper import *
 from .gropt_wrapper import __build_date__
-from . import readasc
+from .readasc import get_random_safe_params
 from .utils import demo, setup_logging
 
 set_log_level(3)  # warn
